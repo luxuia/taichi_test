ab03b66373ff36c6204f0788c0fe2a86ae7c473d
luxuia <luxuia@foxmail.com>
2020-06-07 11:02:16 +0800

    3d versuib

--- 1st.py
+++ 1st.py
@@ -5,9 +5,13 @@ ti.init(ti.cuda)
 n=320
 pixels=ti.var(dt=ti.f32,shape=(n*2,n))
 
-RANDOM_TBL = ti.Vector(2, dt=ti.f32, shape=(n*2,n))
-
 CellSize = 0.1
+
+TEX_COUNT = int(2/CellSize)+1
+
+RANDOM_TBL = ti.Vector(2, dt=ti.f32, shape=(TEX_COUNT,TEX_COUNT))
+RANDOM3_TBL = ti.Vector(3, dt=ti.f32, shape=(
+    TEX_COUNT,TEX_COUNT,TEX_COUNT))
 
 @ti.func
 def rand1dT1d(t:ti.template)->ti.template:
@@ -40,8 +44,6 @@ def gradientNoise(t:ti.template)->ti.f32:
     fraction = t-int(t)
     interpolator = easeInOut(fraction)
 
-    cellNoiseZ = ti.Vector([0,0,0])
-
     #return fraction.dot(fraction)
     #return fraction[0]
     ft = int(t)
@@ -49,7 +51,6 @@ def gradientNoise(t:ti.template)->ti.f32:
 
     lowerleftd = rand1dT1d(ti.Vector([ft[0], ft[1]]))*2-1
     
-
     #return (lowerleftd.dot(ti.Vector([1,1])))/2
 
     #return lowerleftd[1]
@@ -76,27 +77,44 @@ def gradientNoise(t:ti.template)->ti.f32:
     noise = lerp(low, up, interpolator[1])
     return noise
 
+@ti.func
+def gradientNoise3D(t:ti.template)->ti.f32:
+    fraction = t-int(t)
+    interpolator = easeInOut(fraction)
 
-'''
+    cellNoiseZ = ti.Vector([0.0,0.0,0.0])
+
+    # it = int(t)
+
+    #return RANDOM3_TBL[it[0], it[1], it[2]][0]
+    
+    # cell = int(t) + ti.Vector([0, 0, 0])
+    # cellDirection = RANDOM3_TBL[cell[0], cell[1], cell[2]]*2-1
+    # compareVector = fraction - ti.Vector([0, 0, 0])
+    # return cellDirection.dot(compareVector)
+
     for z in ti.static(range(2)):
-        cellNoiseY = ti.Vector([0,0,0])
+        cellNoiseY = ti.Vector([0.0,0.0,0.0])
 
         for y in ti.static(range(2)):
-            cellNoiseX = ti.Vector([0,0,0])
+            cellNoiseX = ti.Vector([0.0,0.0,0.0])
 
             for x in ti.static(range(2)):
                 cell = int(t) + ti.Vector([x, y, z])
-                cellDirection = rand1dT1d(cell)*2-1
+                cellDirection = RANDOM3_TBL[cell[0], cell[1], cell[2]]*2-1
                 compareVector = fraction - ti.Vector([x, y, z])
                 cellNoiseX[x] = cellDirection.dot(compareVector)
+                # return cellDirection.dot(compareVector)
+                #return cellNoiseX[x]
 
             cellNoiseY[y] = lerp(cellNoiseX[0], cellNoiseX[1], interpolator[0])
 
         cellNoiseZ[z] = lerp(cellNoiseY[0], cellNoiseY[1], interpolator[1])
+    
 
     noise = lerp(cellNoiseZ[0], cellNoiseZ[1], interpolator[2])
     return noise
-'''
+
     #return previousLinePoint*interpolator + nextLinePoint*(1-interpolator)
 
 @ti.func
@@ -106,21 +124,37 @@ def complex_sqr(z):
 @ti.kernel
 def paint(t:ti.f32):
     for i,j in pixels:#Parallized over all pixels
-        #while z.norm()<20 and iterations<50:
-        #    z=complex_sqr(z)+c
-        #    iterations+=1
         pos = ti.Vector([i,j])
         pixels[i,j]= gradientNoise(pos/n / CellSize) #1-iterations*0.02
 
 @ti.kernel
+def paint3d(t:ti.f32):
+    for i,j in pixels:#Parallized over all pixels
+        pos = ti.Vector([i,j,t])
+        noise = gradientNoise3D(pos/n / CellSize) #1-iterations*0.02
+        noise += 0.5
+        noise *= 6
+        noise = (noise-int(noise))
+        if noise < 0.9:
+            noise = 0
+        pixels[i,j] = noise
+
+@ti.kernel
 def generate_random():
-    for i,j in pixels:
-        RANDOM_TBL[i,j] = ti.Vector([ti.random(), ti.random()])
+    for i in range(TEX_COUNT):
+        for j in range(TEX_COUNT):
+            RANDOM_TBL[i,j] = ti.Vector([ti.random(), ti.random()])
+            
+    for i in range(TEX_COUNT):
+        for j in range(TEX_COUNT):
+            for k in range(TEX_COUNT):
+                RANDOM3_TBL[i,j, k] = ti.Vector([ti.random(), ti.random(), ti.random()])
 
 generate_random()
 
 gui=ti.GUI("JuliaSet",res=(n*2,n))
 for i in range(1000000):
-    paint(i*0.3)
+    # paint(i*0.3)
+    paint3d((i*0.3) % (n*2))
     gui.set_image(pixels)
     gui.show()
